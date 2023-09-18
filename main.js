b42fc16ad7fcb73024cda6ca711ca15f75d6820b
IgorBal2023 <protuberanec2023@gmail.com>
2023-09-18 22:28:41 +0300

    lesson-20 not cards

--- main.js
+++ main.js
@@ -1,101 +1,207 @@
-const nameTest = () => {
+const newTask = () => {
   console.log(``);
-  console.log(startName);
+  console.log(nameTask);
   console.log(``);
 }
+// =================================================
+let nameTask = `Create arr method`;
+newTask();
 
-// ================================================================================================
-let startName = `days in year`;
-nameTest();
-
-const getDaysInYear = (year) => {
-  const firstDaysInYear = new Date(year, 0, 1);
-  const lastDaysInYear = new Date(year, 12, 1);
-  const allDaysInYear = (lastDaysInYear - firstDaysInYear) / (1000 * 60 * 60 * 24);
-  console.log(allDaysInYear);
+const obj = {
+  from: 1,
+  to: 10,
 };
 
-getDaysInYear(2021);
-// 365
-getDaysInYear(2020);
-// 366
-
-// ================================================================================================
-startName = `day of a year`;
-nameTest();
-
-const getDayNumber = (date) => {
-  const dateYear = new Date(date).getFullYear();
-  const dayOfYear = Math.ceil((new Date(date) - new Date(dateYear, 0, 1)) / (1000 * 60 * 60 * 24));
-  console.log(dayOfYear);
-
+const createArr = () => {
+  const arr = [];
+  for (let i = obj.from; i < obj.to; i++) {
+    arr.push(i);
+  }
+  obj.arr = arr;
 };
+createArr();
+console.log(obj.arr);  // [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// ===========================
 
-getDayNumber("2023-01-12");  // 12
-getDayNumber("2023-02-26");  // 57
-getDayNumber("2021-11-12");
-getDayNumber("1985-01-18");
+function createArr2() {
+  const arrCreate = [];
+  for (i = this.from; i < this.to; i++) {
+    arrCreate.push(i);
+  }
+  this.arr = arrCreate;
+}
+const createArrBind = createArr2.bind(obj);
+createArrBind();
+console.log(obj.arr);
+// ===========================
+function createArr3() {
+  const arr = [];
+  for (i = this.from; i < this.to; i++) {
+    arr.push(i);
+  }
+  this.arr = arr;
+}
+const createArrBind2 = createArr2.bind(obj);
+createArrBind2.apply(obj);
+console.log(obj.arr);
 
-// ================================================================================================
-startName = ` fiscal quarters`;
-nameTest();
+// ====================================================
+nameTask = `Calculator`;
+newTask();
 
-const getQuarters = (date) => {
-  const monthQuarters = new Date(date).getMonth();
-  const dayQuarters = new Date(date).getDate();
-  const monthEnter = new Date(date).toLocaleString('en-US', { month: `short` });
+function Calculator() {
+  this.result = 0;
 
-  if (monthQuarters <= 2) {
-    console.log(monthEnter + ` ` + dayQuarters + ` is I quarter`);
-  }
-  else if (monthQuarters > 2 && monthQuarters <= 5) {
-    console.log(monthEnter + ` ` + dayQuarters + ` is 2 quarter`);
-  }
-  else if (monthQuarters > 5 && monthQuarters <= 8) {
-    console.log(monthEnter + ` ` + dayQuarters + ` is 3 quarter`);
-  }
-  else if (monthQuarters > 8 && monthQuarters <= 11) {
-    console.log(monthEnter + ` ` + dayQuarters + ` is 4 quarter`);
-  }
-};
+  this.sum = function (a) {
+    this.result += a;
+    return this;
+  };
+  this.sub = function (a) {
+    this.result -= a;
+    return this;
+  };
+  this.mul = function (a) {
+    this.result *= a;
+    return this;
+  };
+  this.div = function (a) {
+    this.result /= a;
+    return this;
+  };
+}
+const calculator = new Calculator();
+calculator.sum(2).sum(2).mul(4);       // 16
+console.log(calculator.result);
+calculator.result = 0;
 
-getQuarters("2023-02-26");   // Feb 26 is I quarter
-getQuarters("2023-04-26");
-getQuarters("2023-09-26");
-getQuarters("2023-11-26");
+calculator.sub(15).sum(2).div(4);
+console.log(calculator.result);
+calculator.result = 0;
 
-// ================================================================================================
-startName = ` a function to calculate date diff`;
-nameTest();
+calculator.mul(20).div(12).sub(4);
+console.log(calculator.result);
+calculator.result = 0;
 
-const calcDateDiff = (startDate, endDate) => {
-  const differenceInTimeMS = new Date(endDate).getTime() - new Date(startDate).getTime();
-  const differenceYear = new Date(endDate).getFullYear() - new Date(startDate).getFullYear();
-  const differenceMonth = new Date(endDate).getMonth() - new Date(startDate).getMonth();
-  const differenceDay = new Date(endDate).getDate() - new Date(startDate).getDate();
-  const differenceInTimeMin = differenceInTimeMS / (1000 * 60);
-  const differenceInTimeHour = Math.floor(differenceInTimeMS / (1000 * 60 * 60));
-  const differenceInTimeHourMin = differenceInTimeMin % 60;
+calculator.div(202).sum(2).sub(4).mul(6);
+console.log(calculator.result);
+calculator.result = 0;
 
-  if (differenceYear !== 0) {
-   return differenceYear + ` years`;
-  }
-  else if (differenceMonth !== 0) {
-    return differenceMonth + ` months`;
-  }
-  else if (differenceDay !== 0) {
-    return differenceDay + ` days`;
-  }
-  else if (differenceInTimeHour !== 0) {
-    return differenceInTimeHour + ` hours ` + differenceInTimeHourMin + ` minutes`;
-  }
-  else if (differenceInTimeMin !== 0) {
-    return differenceInTimeMin + ` minutes`;
-  }
-};
+// ====================================================
+nameTask = `Constructor Function`;
+newTask();
 
-console.log(calcDateDiff("2023-02-26 14:00", "2023-02-26 14:20"));   // 20 minutes
-console.log(calcDateDiff("2023-02-26 14:00", "2023-02-26 15:30"));   // 1 hours 30 minutes
-console.log(calcDateDiff("2023-02-26 14:00", "2023-02-28 15:30"));   // 2 days
-console.log(calcDateDiff("2023-02-26 14:00", "2023-05-28 15:30"));   // 3 months
-console.log(calcDateDiff("2023-02-26 14:00", "2025-05-28 15:30"));   // 2 years
+function Car(model, color, age, speed, gasTank, started) {
+  this.model = model;
+  this.color = color;
+  this.age = age;
+  this.speed = speed;
+  this.gasTank = gasTank;
+  this.engineStarted = started;
+
+
+  //========== STARTENGINE =========== 
+  this.startEngine = function () {
+    if (this.gasTank !== 0) {
+      this.engineStarted = true;
+      console.log(`Engine start`);
+      return this;
+    }
+    else {
+      this.engineStarted = false;
+      console.log(`No fuel!!!`);
+      return this;
+    }
+
+  }
+  //==========  DRIVE =========== 
+  this.drive = function () {
+    if (this.engineStarted !== false && this.gasTank !== 0) {
+      this.drive = true;
+      this.speed = 30;
+      console.log(`Drive - ` + this.drive);
+      return this;
+    }
+    else {
+      this.drive = false;
+      console.log(`Drive - ` + this.drive);
+      return this;
+    }
+  }
+  //==========  STOP =========== 
+  this.stop = function () {
+    if (this.engineStarted == false || this.speed == 0) {
+      this.speed = 0;
+      this.engineStarted = false;
+      console.log(`Car engine stop!`);
+      return this;
+    }
+    else {
+      console.log(`Car move!`);
+      return this;
+    }
+  }
+  //==========  SPEEDUP =========== 
+  this.speedUp = function (arg) {
+    const maxSpeed = 200;
+    const fuelExpense = 5;
+    let speedUpItter = Math.trunc(this.gasTank / fuelExpense);
+    let speed_Up = this.speed;
+    if (this.drive == true) {
+
+      for (let i = 1; i <= speedUpItter; i++) {
+        speed_Up += arg;
+        this.speedUp = speed_Up;
+        this.speed = this.speedUp;
+        console.log(`Speed ` + this.speedUp + `!`);
+      }
+      console.log(`No fuel, the car slows down!!!`);
+      return this;
+    }
+    else {
+      console.log(`Car stop!`);
+      return this;
+    }
+  }
+  //==========  SLOWDOWN =========== 
+  this.slowDown = function (arg) {
+    let slowDownItter = Math.trunc(this.speed / arg);
+    if (this.speed !== 0) {
+      for (let i = slowDownItter; i > 0; i--) {
+        this.speed -= arg;
+        this.slowDown = this.speed
+        console.log(`Speed` + this.slowDown + `!`);
+      }
+      console.log(`Car stopped!`);
+      return this;
+    }
+    else {
+      console.log(`Car stop!`);
+      return this;
+    }
+  }
+  // ===== GAS =====
+  this.addGas = function (arg) {
+    const maxGas = 20;
+    const stepGas = arg;
+    if (this.gasTank < maxGas) {
+      const maxItterGas = Math.trunc(maxGas / stepGas);
+      let fuelTank = this.gasTank;
+
+      if (fuelTank + stepGas < maxGas) {
+        for (let i = 1; i <= maxItterGas; i++) {
+          fuelTank += stepGas;
+        }
+        this.gasTank = fuelTank;
+      }
+      console.log(`In tank ` + this.gasTank + ` units gas!`);
+    }
+    else {
+      console.log(`Gas tank is FULL!`);
+    }
+    return this;
+  }
+}
+
+const car = new Car(`Mazda`, `white`, `10`, 0, 0);
+car.addGas(5).startEngine().drive().speedUp(5).slowDown(5).stop();
+
