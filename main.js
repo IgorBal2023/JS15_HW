ed8b5a37790ca4834af999bd1f11517ac6ae0d30
IgorBal2023 <protuberanec2023@gmail.com>
2023-09-26 16:34:41 +0300

    arr in arr version no comments

--- main.js
+++ main.js
@@ -58,67 +58,67 @@ for (i = 0; i < faculty.groups.length; i++) {
   console.log(`Group name: ` + groupName);
   console.log(`Group members: ` + studentsName); // [['Taras']]
 }
-// // ======================================
-// nameExercise = `Prototype constructor`;
-// start();
+// ======================================
+nameExercise = `Prototype constructor`;
+start();
 
-// function Animal(name) {
-//   this.name = name;
-// }
-// Animal.prototype.getInfo = function () {
-//   return this.name;
-// };
+function Animal(name) {
+  this.name = name;
+}
+Animal.prototype.getInfo = function () {
+  return this.name;
+};
 
-// function Mammal(name, live) {
-//   Animal.call(this, name);
-//   this.live = live;
-// }
-// Mammal.prototype = Object.create(Animal.prototype);
-// Mammal.prototype.run = function () {
-//   return this.name + ` run`;
-// };
+function Mammal(name, live) {
+  Animal.call(this, name);
+  this.live = live;
+}
+Mammal.prototype = Object.create(Animal.prototype);
+Mammal.prototype.run = function () {
+  return this.name + ` run`;
+};
 
-// function Bird(name, live) {
-//   Animal.call(this, name);
-//   this.live = live;
-// }
-// Bird.prototype = Object.create(Animal.prototype);
-// Bird.prototype.fly = function () {
-//   return this.name + ` fly`;
-// };
+function Bird(name, live) {
+  Animal.call(this, name);
+  this.live = live;
+}
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.fly = function () {
+  return this.name + ` fly`;
+};
 
-// function Dog(name, live, breed) {
-//   Animal.call(this, name, live);
-//   this.breed = breed;
-// }
-// Dog.prototype = Object.create(Mammal.prototype);
-// Dog.prototype.whoIsGoodDog = function () {
-//   return this.name + ` barked gaf-gaf`;
-// };
+function Dog(name, live, breed) {
+  Animal.call(this, name, live);
+  this.breed = breed;
+}
+Dog.prototype = Object.create(Mammal.prototype);
+Dog.prototype.whoIsGoodDog = function () {
+  return this.name + ` barked gaf-gaf`;
+};
 
-// function Penguin(name, live, species) {
-//   Animal.call(this, name, live);
-//   this.species = species;
-// }
-// Penguin.prototype = Object.create(Bird.prototype);
-// Penguin.prototype.fly = function () {
-//   return this.name + ` don\`t fly`;
-// };
-// Penguin.prototype.swim = function () {
-//   return this.name + ` swim`;
-// };
+function Penguin(name, live, species) {
+  Animal.call(this, name, live);
+  this.species = species;
+}
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.fly = function () {
+  return this.name + ` don\`t fly`;
+};
+Penguin.prototype.swim = function () {
+  return this.name + ` swim`;
+};
 
-// const animal = new Animal(`Dog`);
-// const mammal = new Mammal(`Humman`, `Hous`);
-// const bird = new Bird(`Eagle`, `in mountains`);
-// const dog = new Dog(`Dog`, `Dog hous`, `Shephard`);
-// const penguin = new Penguin(`Penguin`, `Antarctic`, `Imperor pinguin`);
+const animal = new Animal(`Dog`);
+const mammal = new Mammal(`Humman`, `Hous`);
+const bird = new Bird(`Eagle`, `in mountains`);
+const dog = new Dog(`Dog`, `Dog hous`, `Shephard`);
+const penguin = new Penguin(`Penguin`, `Antarctic`, `Imperor pinguin`);
 
-// console.log(animal.getInfo());
-// console.log(mammal.getInfo());
-// console.log(bird.getInfo());
-// console.log(mammal.run());
-// console.log(bird.fly());
-// console.log(dog.whoIsGoodDog());
-// console.log(penguin.fly());
-// console.log(penguin.swim());
+console.log(animal.getInfo());
+console.log(mammal.getInfo());
+console.log(bird.getInfo());
+console.log(mammal.run());
+console.log(bird.fly());
+console.log(dog.whoIsGoodDog());
+console.log(penguin.fly());
+console.log(penguin.swim());
